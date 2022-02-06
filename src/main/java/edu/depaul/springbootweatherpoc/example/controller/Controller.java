d0ace9e07d34ddde48354b20f42bd66e6a4717ba
Joseph Varilla <jvarilla981@gmail.com>
2022-02-06 14:06:09 -0600

    Adds test data and configures the app to use h2

--- src/main/java/edu/depaul/springbootweatherpoc/example/controller/Controller.java
+++ src/main/java/edu/depaul/springbootweatherpoc/example/controller/Controller.java
@@ -2,13 +2,21 @@ package edu.depaul.springbootweatherpoc.example.controller;
 
 import edu.depaul.springbootweatherpoc.example.constants.Constants;
 import edu.depaul.springbootweatherpoc.example.model.DataResponse;
+import edu.depaul.springbootweatherpoc.weather.model.TestEntity;
+import edu.depaul.springbootweatherpoc.weather.repository.TestRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 public class Controller {
+    @Autowired
+    private TestRepository testRepository;
+
     @GetMapping("/greetings")
     public String index() {
         return "Greetings from Spring Boot!";
@@ -16,8 +24,9 @@ public class Controller {
 
     @GetMapping("/data")
     public ResponseEntity<DataResponse> getSampleData(){
+        List<TestEntity> testEntityList = testRepository.findAll();
         DataResponse response = new DataResponse();
-        String data = "This is the data. This can be any data type.";
+        String data = testEntityList.toString(); //"This is the data. This can be any data type.";
         response.setStatusCode(HttpStatus.OK.value());
         response.setStatusDescription(Constants.SUCCESS_STATUS_MESSAGE);
         response.setData(data);
